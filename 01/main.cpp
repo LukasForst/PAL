39801c28ab8a1d0124b802067d22ef4588b54a7d
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-06 12:28:54 +0200

    wip hw1

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,9 +1,70 @@
 #include <iostream>
+#include <list>
+#include <vector>
+
 using namespace std;
 
-int main(int argc, char const *argv[])
+struct Vertex
 {
-    /* code */
-    cout << "Hello, World!" << endl;
+    int id;
+    int potential;
+    int r;
+    int c;
+    Vertex *parent;
+};
+
+bool isZeroPotential(Vertex &v)
+{
+    return v.potential == 0;
+}
+
+void fillMesh(int R, int C, vector<Vertex> &data)
+{
+    for(int r = 0; r < R; r++)
+    {
+        for(int c = 0; c < C; c++){
+            int id = r*C+c;
+
+            data[id].id = id;
+            data[id].r = r;
+            data[id].c = c;
+            data[id].potential = 0;
+        }
+    }
+    return;
+}
+
+void nonZeroPotentialVertexes(int P, int C, vector<Vertex> &data)
+{
+    for (int i = 0; i < P; i++)
+    {
+        int r, c, p;
+        fscanf(stdin, "%d %d %d", &r, &c, &p);
+        int id = r*C+c;
+        data[id].id = id;
+        data[id].r = r;
+        data[id].c = c;
+        data[id].potential = p;
+    }
+    return;
+}
+
+int run()
+{
+    int R, C, P, K;
+    fscanf(stdin, "%d %d %d %d", &R, &C, &P, &K);
+    
+    vector<Vertex> vertices(R * C);
+    fillMesh(R, C, vertices);
+    nonZeroPotentialVertexes(P, C, vertices);
+
+
+
+    return 0;
+}
+
+int main() {
+    vector<Vertex> v(2);
+    cout << v[0].potential;
     return 0;
 }
