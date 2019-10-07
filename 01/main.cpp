4e88ca5f41872410751d99e197eb0a54e248344f
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-07 23:32:25 +0200

    add bfs and potential discovery

--- 01/main.cpp
+++ 01/main.cpp
@@ -2,8 +2,11 @@
 #include <list>
 #include <vector>
 #include <map>
+#include <queue>
 
 using namespace std;
+
+int R, C, P, K;
 
 struct Vertex
 {
@@ -11,13 +14,16 @@ struct Vertex
     int potential;
     int r;
     int c;
-    Vertex *nearest_nzp_vertex;
+
+    int nearest_nzp_vertex_idx;
     int distance_to_nzp_vertex;
+
+    bool is_nzpv;
 
     list<int> adjencent;
 };
 
-void fill_nearest(int r, int c, int R, int C, list<int> &adjencent)
+void fill_nearest(int r, int c, list<int> &adjencent)
 {
     if (r + 1 != R)
     {
@@ -34,42 +40,51 @@ void fill_nearest(int r, int c, int R, int C, list<int> &adjencent)
         adjencent.push_back(r * C + (c + 1));
     }
 
-    if (c - 1 !=  -1)
+    if (c - 1 != -1)
     {
         adjencent.push_back(r * C + (c - 1));
     }
 }
 
-void fill_mesh(int R, int C, vector<Vertex> &data)
+void fill_mesh(vector<Vertex> &data)
 {
     for (int r = 0; r < R; r++)
     {
         for (int c = 0; c < C; c++)
         {
             int id = r * C + c;
-            Vertex *v = &data[id];
-            v->id = id;
-            v->r = r;
-            v->c = c;
-            v->potential = 0;
-            fill_nearest(r, c, R, C, v->adjencent);
+            Vertex &v = data[id];
+            v.id = id;
+            v.r = r;
+            v.c = c;
+            v.potential = 0;
+            v.distance_to_nzp_vertex = INT32_MAX - 2;
+            v.is_nzpv = false;
+            fill_nearest(r, c, v.adjencent);
         }
     }
 }
 
-void nzp_vertices(int P, int C, vector<Vertex> &data)
+vector<int> nzp_vertices(vector<Vertex> &data)
 {
+    vector<int> nzps(P);
     for (int i = 0; i < P; i++)
     {
         int r, c, p;
         fscanf(stdin, "%d %d %d", &r, &c, &p);
         int id = (r - 1) * C + (c - 1);
+        auto &v = data[id];
+        v.is_nzpv = true;
+        v.distance_to_nzp_vertex = 0;
+        v.nearest_nzp_vertex_idx = id;
+        v.potential = p;
 
-        data[id].potential = p;
+        nzps[i] = id;
     }
+    return nzps;
 }
 
-void extra_edges(int K, int C, vector<Vertex> &data)
+void extra_edges(vector<Vertex> &data)
 {
     for (int i = 0; i < K; i++)
     {
@@ -84,23 +99,82 @@ void extra_edges(int K, int C, vector<Vertex> &data)
     }
 }
 
+void print_vertex(int number, vector<Vertex> &vertices)
+{
+    int idx = number - 1;
+    auto &v = vertices[idx];
+    cout << "Vertex No: " << idx + 1;
+    cout << " distance: " << v.distance_to_nzp_vertex;
+    cout << " potential: " << v.potential;
+    cout << " nzpv: " << v.nearest_nzp_vertex_idx + 1 << "\n";
+    for (auto &adj : v.adjencent)
+    {
+        cout << adj + 1 << "\n";
+    }
+}
+
+void change_potential(Vertex &to, Vertex &from, queue<int> &q)
+{
+    if (to.is_nzpv)
+    {
+        return;
+    }
+
+    to.distance_to_nzp_vertex = from.distance_to_nzp_vertex + 1;
+    to.nearest_nzp_vertex_idx = from.nearest_nzp_vertex_idx;
+    to.potential = from.potential;
+    q.emplace(to.id);
+}
+
+void explore_vertex(int idx, queue<int> &q, vector<Vertex> &data)
+{
+    auto &v = data[idx];
+    for (int adj_idx : v.adjencent)
+    {
+        auto &adj = data[adj_idx];
+
+        if (adj.distance_to_nzp_vertex > v.distance_to_nzp_vertex + 1)
+        {
+            change_potential(adj, v, q);
+        }
+        else if (adj.distance_to_nzp_vertex == v.distance_to_nzp_vertex + 1)
+        {
+            if (data[adj.nearest_nzp_vertex_idx].potential > data[v.nearest_nzp_vertex_idx].potential)
+            {
+                change_potential(adj, v, q);
+            }
+        }
+    }
+}
+
+void set_nzpv(vector<int> &nzps, vector<Vertex> &data)
+{
+    queue<int> q;
+    for (int nzp_idx : nzps)
+    {
+        q.emplace(nzp_idx);
+    }
+
+    while (!q.empty())
+    {
+        int to_explore_idx = q.front();
+        q.pop();
+        explore_vertex(to_explore_idx, q, data);
+    }
+}
 
 int run()
 {
-    int R, C, P, K;
     fscanf(stdin, "%d %d %d %d", &R, &C, &P, &K);
 
     vector<Vertex> vertices(R * C);
-    fill_mesh(R, C, vertices);
-    nzp_vertices(P, C, vertices);
-    extra_edges(K, C, vertices);
+    fill_mesh(vertices);
+    vector<int> nzps = nzp_vertices(vertices);
+    extra_edges(vertices);
 
-    int idx = 5;
-    cout << "pot: " << vertices[idx].potential << "\n";
-    for (auto &adj: vertices[idx].adjencent) {
-        cout << adj + 1 << "\n";
-    }
+    set_nzpv(nzps, vertices);
 
+    print_vertex(5, vertices);
     return 0;
 }
 
