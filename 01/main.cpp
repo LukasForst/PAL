42f4800b722237651354d85f4f4c015af45e71d5
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-09 17:31:04 +0200

    finally sorted edges

--- 01/main.cpp
+++ 01/main.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include <cstdlib>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -209,6 +210,100 @@ void create_weighted_edges(list<Edge> &edges, const vector<Vertex> &data)
     }
 }
 
+// A structure to represent a subset for union-find  
+class subset  
+{  
+    public: 
+    int parent;  
+    int rank;  
+};  
+  
+// A utility function to find set of an element i  
+// (uses path compression technique)  
+int find(subset subsets[], int i)  
+{  
+    // find root and make root as parent of i  
+    // (path compression)  
+    if (subsets[i].parent != i)  
+        subsets[i].parent = find(subsets, subsets[i].parent);  
+  
+    return subsets[i].parent;  
+}  
+  
+// A function that does union of two sets of x and y  
+// (uses union by rank)  
+void Union(subset subsets[], int x, int y)  
+{  
+    int xroot = find(subsets, x);  
+    int yroot = find(subsets, y);  
+  
+    // Attach smaller rank tree under root of high  
+    // rank tree (Union by Rank)  
+    if (subsets[xroot].rank < subsets[yroot].rank)  
+        subsets[xroot].parent = yroot;  
+    else if (subsets[xroot].rank > subsets[yroot].rank)  
+        subsets[yroot].parent = xroot;  
+  
+    // If ranks are same, then make one as root and  
+    // increment its rank by one  
+    else
+    {  
+        subsets[yroot].parent = xroot;  
+        subsets[xroot].rank++;  
+    }  
+}  
+  
+// The main function to construct MST using Kruskal's algorithm  
+void KruskalMST(const list<Edge> &edges_data)  
+{  
+    vector<Edge> edges(edges_data.begin(), edges_data.end());
+
+    int E = edges.size;
+    int V = R*C;
+    Edge result[V]; // Tnis will store the resultant MST  
+    int e = 0; // An index variable, used for result[]  
+    int i = 0; // An index variable, used for sorted edges  
+
+    // Allocate memory for creating V ssubsets  
+    subset *subsets = new subset[( V * sizeof(subset) )];  
+  
+    // Create V subsets with single elements  
+    for (int v = 0; v < V; ++v)  
+    {  
+        subsets[v].parent = v;  
+        subsets[v].rank = 0;  
+    }  
+  
+    // Number of edges to be taken is equal to V-1  
+    while (e < V - 1 && i < E)  
+    {  
+        // Step 2: Pick the smallest edge. And increment  
+        // the index for next iteration  
+        Edge next_edge = edges[i++];  
+  
+        int x = find(subsets, next_edge.src);  
+        int y = find(subsets, next_edge.dest);  
+  
+        // If including this edge does't cause cycle,  
+        // include it in result and increment the index  
+        // of result for next edge  
+        if (x != y)  
+        {  
+            result[e++] = next_edge;  
+            Union(subsets, x, y);  
+        }  
+        // Else discard the next_edge  
+    }  
+  
+    // print the contents of result[] to display the  
+    // built MST  
+    cout<<"Following are the edges in the constructed MST\n";  
+    for (i = 0; i < e; ++i)  
+        cout<<result[i].src<<" -- "<<result[i].dest<<" == "<<result[i].weight<<endl;  
+    return;  
+}  
+
+
 int run()
 {
     fscanf(stdin, "%d %d %d %d", &R, &C, &P, &K);
@@ -228,6 +323,7 @@ int run()
     cout << "sorted:\n";
     edges.sort([](const Edge &e1, const Edge &e2){ return e1.weight <= e2.weight; });
 
+    KruskalMST(edges);
 
     // for(const auto &e : edges) {
     //     cout << e.src + 1 << ":" << e.dest + 1 << " - " << e.weight << "\n";
