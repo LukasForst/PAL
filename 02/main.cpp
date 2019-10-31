1a7a4657c9840541717239e519f5dd16cb2b5038
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-31 21:47:55 +0100

    prepare for second assignment

---
+++ 02/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <map>
+#include <queue>
+#include <cstdlib>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+int main()
+{
+    fscanf(stdin, "%d %d", &N, &M);
+    vector<list<int>> adjency_list(N);
+    
+    for(auto i = 0; i < M; i++) 
+    {
+        int from, to;
+        fscanf(stdin, "%d %d", &from, &to);
+        adjency_list[from].push_back(to);
+    }
+
+    return 0;
+}
