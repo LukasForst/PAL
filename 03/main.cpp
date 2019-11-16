888f9165ca6d179bd313fc2287d2cbb3da250507
Lukas Forst <lukas.forst@blindspot.ai>
2019-11-16 10:46:15 +0100

    cycle detection

--- 03/main.cpp
+++ 03/main.cpp
@@ -18,9 +18,15 @@ using namespace std;
 // M and is the number of edges of each graph
 int T, N, M;
 
+enum ColoringState {
+    NEW, PARTIALLY_VISITED, COMPLETELY_VISITED
+};
+
 struct Node {
     string certificate = "C";
     bool in_cycle = false;
+    bool removed = false;
+    ColoringState color = NEW;
     list<int> adjacent;
 };
 
@@ -30,6 +36,50 @@ struct Graph {
 };
 
 vector<Graph> graphs;
+
+void dfs_cycle(int u, int p, vector<int> &color, vector<int> &par, vector<Node> &nodes) {
+
+    auto &current = nodes[u];
+    // already (completely) visited vertex.
+    if (current.color == COMPLETELY_VISITED) {
+        return;
+    }
+
+    // seen vertex, but was not completely visited -> cycle detected.
+    // backtrack based on parents to find the complete cycle.
+    if (current.color == PARTIALLY_VISITED) {
+        int cur = p;
+        nodes[cur].in_cycle = true;
+        // backtrack the vertex which are
+        // in the current cycle thats found
+        while (cur != u) {
+            cur = par[cur];
+            nodes[cur].in_cycle = true;
+        }
+        return;
+
+    } else {
+        par[u] = p;
+
+        // partially visited.
+        current.color = PARTIALLY_VISITED;
+
+        // simple dfs on graph
+        for (int v : current.adjacent) {
+
+            // if it has not been visited previously
+            if (v == par[u]) {
+                continue;
+            }
+
+            dfs_cycle(v, u, color, par, nodes);
+        }
+
+        // completely visited.
+        current.color = COMPLETELY_VISITED;
+    }
+}
+
 
 int main() {
     fscanf(stdin, "%d %d %d", &T, &N, &M);
@@ -47,6 +97,17 @@ int main() {
             g.nodes[from_idx].adjacent.push_back(to_idx);
             g.nodes[to_idx].adjacent.push_back(from_idx);
         }
+
+        vector<int> color(N);
+        vector<int> parents(N);
+        dfs_cycle(0, -1,color, parents, g.nodes);
+        cout << "Nodes in cycle: ";
+        for(int i = 0; i < N; i++) {
+            if(g.nodes[i].in_cycle) {
+                cout << i << ", ";
+            }
+        }
+        cout << endl;
     }
 
 
