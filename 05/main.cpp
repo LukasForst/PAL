1ffb4f1189cbf7f5cc16899008818e5c49f929a0
Lukas Forst <lukas.forst@blindspot.ai>
2019-12-08 17:20:57 +0100

    add hw05

---
+++ 05/main.cpp
@@ -0,0 +1,14 @@
+#include <list>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cfloat>
+#include <limits>
+#include <set>
+
+#define DEBUG 0
+using namespace std;
+
+
+int main() {
+}
